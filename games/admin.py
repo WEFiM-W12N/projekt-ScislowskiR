94e1274dbf9c979ebd327b7af512ff6ceacedb79
Matiios <matiius11@gmail.com>
2022-04-03 12:11:28 +0200

    created Game Type, Game Modes classes with its nested enum classes and fields with availability choices to display. Created Game class with foreign key defined many-to-one relationship and other fields. Used comments for a better look.

--- games/admin.py
+++ games/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Game
+from .models import Game, Game_Modes, Game_Type
 # Register your models here.
+
+admin.site.register(Game_Type)
 admin.site.register(Game)
+admin.site.register(Game_Modes)
--- games/models.py
+++ games/models.py
@@ -1,21 +1,12 @@
 from enum import Enum
 from django.db import models
 
-class Game(models.Model):
-    
+
+class Game_Type(models.Model):
     """
-    Nested Choice Class 
+        Nested Enum Class to display available choices of Game Type 
     """
-    class Game_Mode(Enum):
-        singleplayer = (1, 'Singleplayer')
-        multiplayer = (2, 'Multiplayer')
-
-        @classmethod
-        def get_enum_value(cls, member):
-            return member.value[0]
-
-
-    class Game_Type(Enum):
+    class Game_Type_Enum(Enum):
         FTP = (1, 'First Person Shooter')
         TTP = (2, 'Third Person Shooter')
         strategy = (3, 'Strategy')
@@ -27,22 +18,60 @@ class Game(models.Model):
         @classmethod
         def get_enum_value(cls, member):
             return member.value[0]
+    """
+        Fields and methods of the Game Type Class
+    """
 
+    type = models.IntegerField(
+                    choices=[x.value for x in Game_Type_Enum],
+                    default=None
+                    )
+
+    def __str__(self):
+        return self.type
+
+    class Meta:
+        verbose_name = "Game Type"
+
+class Game_Modes(models.Model):  
+    """
+        Nested Enum Class to display available choices of Game Type 
+    """
+    class Game_Modes_Enum(Enum):
+        singleplayer = (1, 'Singleplayer')
+        multiplayer = (2, 'Multiplayer')
+
+        @classmethod
+        def get_enum_value(cls, member):
+            return member.value[0]
 
     """
-    Class Fields
+        Fields and methods of the Game Type Class
+    """  
+    mode = models.IntegerField(
+                        choices=[x.value for x in Game_Modes_Enum],
+                        default=None)
+
+
+    def __str__(self):
+        return self.mode
+    
+    class Meta:
+        verbose_name = "Game Mode"
+
+class Game(models.Model):
     """
+        Fields and methods of the Game Type Class
+    """
+    game_type = models.ForeignKey(Game_Type, on_delete=models.CASCADE,blank=True, null=True)
+    game_mode = models.ForeignKey(Game_Modes, on_delete=models.CASCADE,blank=True, null=True)
     title = models.CharField(max_length=50)
     production_year = models.SmallIntegerField(default=None)
     description = models.TextField(max_length=200)
-    mode = models.IntegerField(
-                            choices=[x.value for x in Game_Mode],
-                            default=None)
-    type = models.IntegerField(
-                            choices=[x.value for x in Game_Type],
-                            default=None
-                            )
 
     def __str__(self):
         return self.title
+    
+    class Meta:
+        verbose_name = "Title"
 
