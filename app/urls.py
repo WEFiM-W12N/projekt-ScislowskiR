e486b92eff5673eeb91d490d5aecdcfa1af611d5
Matiios <matiius11@gmail.com>
2022-04-03 15:46:29 +0200

    created class views  and connect game_view to the urls

--- app/urls.py
+++ app/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from games.views import Game_Modes_View, Game_Type_View, Game_View
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('games/', include('games.urls', namespace='games')),
+    
+    
 ]
--- games/views.py
+++ games/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from multiprocessing import context
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from .models import * 
 
-# Create your views here.
+
+class Game_Type_View(generic.ListView):
+    def index(request):
+        game_type = Game_Type.objects.all()
+        return HttpResponse(game_type)
+
+class Game_Modes_View(generic.ListView):
+    def index(request):
+        game_mode = Game_Modes.objects.all()
+        return HttpResponse(game_mode)
+
+class Game_View(generic.TemplateView):
+    template_name = "game_index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['games'] = Game.objects.all()
+        return context
+
+
