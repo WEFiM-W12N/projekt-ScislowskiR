77ea74256c4085082dae786100ea57760c2e199e
Matiios <matiius11@gmail.com>
2022-04-07 23:01:08 +0200

    created another view class 'GameListView' which is responsible to display titles of games by their types. Created game_list_index.html to render the objects

--- games/views.py
+++ games/views.py
@@ -1,5 +1,6 @@
 
 from django.db.models import F
+from django.shortcuts import render
 from django.views import generic
 
 from .models import * 
@@ -13,3 +14,11 @@ class TypeView(generic.ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+
+class GameListView(generic.ListView):
+    model = Game
+    paginate_by = 100  # Amount of shown objects
+    template_name = 'game_list_index.html'
+    context_object_name = 'games_list'
+    def get_queryset(self):
+        return Game.objects.filter(title=self.kwargs['title']).order_by('title')
