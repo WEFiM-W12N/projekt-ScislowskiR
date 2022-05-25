9ef2693f12b78410340371dd061b0e2b8aae3165
Matiios <matiius11@gmail.com>
2022-05-25 15:44:14 +0200

    upload games/urls.py to repository

---
+++ games/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import *
+
+
+app_name = 'games'
+
+urlpatterns = [
+    path('types', TypeView.as_view(), name='type-list'),
+    path('titles', ListView.as_view(), name='type-list'),
+    path('<slug:slug>/', DetailView.as_view(), name='type-list'),
+]
+
